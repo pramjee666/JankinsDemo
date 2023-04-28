7b67abe54cc0cf2078e083af0d6480efdc4074e0
Ram Pandit <pramjee666@gmail.com>
2023-04-28 23:09:12 +0530

    My first commit

---
+++ src/main/java/com/cg/jenkindemo/JenkinDemo/App.java
@@ -0,0 +1,7 @@
+package com.cg.jenkindemo.JenkinDemo;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/cg/jenkindemo/JenkinDemo/AppTest.java
@@ -0,0 +1,13 @@
+package com.cg.jenkindemo.JenkinDemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
